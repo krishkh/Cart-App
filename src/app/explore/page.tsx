a804d1ee9eb7c0f6d0b76686df1269dee45e3249
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-24 02:34:03 +0530

    trynna fix

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,32 @@
+import ItemCard from "../../components/Item related components/itemCard";
+import initData from "../../../public/init/initData.json";
+
+interface ItemData {
+  itemNumber: number;
+  itemName: string;
+  itemPrice: number;
+  category: string;
+  description: string;
+}
+
+interface ExploreProps {
+  data?: ItemData[];  // Make data optional since you are providing a default value
+}
+
+// Assuming initData is correctly typed as ItemData[]
+const typedData: ItemData[] = initData;
+
+const Explore: React.FC<ExploreProps> = ({ data = typedData }) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
+      <div className="dark:text-gray-400 mt-8 flex-wrap dark:bg-blackaf body-font flex container px-5 py-5 mx-auto">
+        {data.map((item) => (
+          <ItemCard key={item.itemNumber} data={item} />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+
+export default Explore;
