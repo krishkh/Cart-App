a8fc104f2e8c0bab73bead8afa4aab9686ec32fc
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-24 15:04:33 +0530

    please work

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -35,8 +35,21 @@
 import React from "react";
 import ItemCard from "../../components/Item related components/itemCard";
 import initData from "../../../public/init/initData.json";
+
+interface Data {
+  data?:
+    | {
+        itemName: string;
+        itemNumber: number;
+        itemPrice: number;
+        category: string;
+        description: string;
+      }[]
+    | undefined;
+}
+
 const typedData = initData;
-const Explore = ({ data = typedData }) => {
+const Explore = ({ data = typedData }: Data) => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
       <div className="dark:text-gray-400 mt-8 flex-wrap dark:bg-blackaf body-font flex container px-5 py-5 mx-auto">
