a8757b4f25ba90825a21e49fa9d28cd8703f7fb1
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-24 14:51:53 +0530

    testing

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -33,11 +33,15 @@
 // export default Explore;
 
 import React from "react";
-
-const Explore = () => {
+import ItemCard from "../../components/Item related components/itemCard";
+import initData from "../../../public/init/initData.json";
+const typedData = initData;
+const Explore = ({data = typedData}) => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
-      This is the Explore page
+      <div className="dark:text-gray-400 mt-8 flex-wrap dark:bg-blackaf body-font flex container px-5 py-5 mx-auto">
+        <ItemCard key={data[0].itemNumber} data={data[0]} />
+      </div>
     </main>
   );
 };
