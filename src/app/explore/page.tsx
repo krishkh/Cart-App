a257e19fba5873ccaeda324775f00b6863910ef5
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-24 15:11:30 +0530

    please work

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -35,23 +35,24 @@
 import React from "react";
 import ItemCard from "../../components/Item related components/itemCard";
 import initData from "../../../public/init/initData.json";
-interface ItemData{
-  itemName: string;
-  itemNumber: number;
-  itemPrice: number;
-  category: string;
-  description: string;
-}
-interface DataList {
-  data?: ItemData[] | undefined;
-}
+// interface ItemData{
+//   itemName: string;
+//   itemNumber: number;
+//   itemPrice: number;
+//   category: string;
+//   description: string;
+// }
+// interface DataList {
+//   data?: ItemData[] | undefined;
+// }
 
 const typedData = initData;
-const Explore = ({ data = typedData }: DataList) => {
+const Explore = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
       <div className="dark:text-gray-400 mt-8 flex-wrap dark:bg-blackaf body-font flex container px-5 py-5 mx-auto">
-        <ItemCard key={data[0].itemNumber} data={data[0]} />
+        <div className="lg:w-1/3 md:w-1/2 p-8 w-full card-shadow">
+        hello please work</div>
       </div>
     </main>
   );
