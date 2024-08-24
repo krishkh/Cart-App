505ee8f16b00ef0c1f5be8e9f11226d0b774dd38
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-24 15:15:47 +0530

    please work

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -35,24 +35,32 @@
 import React from "react";
 import ItemCard from "../../components/Item related components/itemCard";
 import initData from "../../../public/init/initData.json";
-// interface ItemData{
-//   itemName: string;
-//   itemNumber: number;
-//   itemPrice: number;
-//   category: string;
-//   description: string;
-// }
+interface ItemData{
+  itemName: string;
+  itemNumber: number;
+  itemPrice: number;
+  category: string;
+  description: string;
+}
 // interface DataList {
 //   data?: ItemData[] | undefined;
 // }
-
+const details: ItemData =  {
+  "itemName": "Wallet",
+  "itemNumber": 9,
+  "itemPrice": 32.5,
+  "category": "Accessories",
+  "description": "A practical and stylish wallet to keep your essentials organized."
+}
 const typedData = initData;
-const Explore = () => {
+const Explore = (data: ItemData = details) => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
       <div className="dark:text-gray-400 mt-8 flex-wrap dark:bg-blackaf body-font flex container px-5 py-5 mx-auto">
+        
         <div className="lg:w-1/3 md:w-1/2 p-8 w-full card-shadow">
         hello please work</div>
+        
       </div>
     </main>
   );
